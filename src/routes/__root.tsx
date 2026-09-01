cf1c84c990ffc58d209cd290056b5795c35af5a8
Matt <64214044+kvbc@users.noreply.github.com>
2026-09-01 11:01:10 +0200

    ts start

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 import Footer from '../components/Footer'
+import { Analytics } from "@vercel/analytics/react"
 
 import '../styles.css'
 
@@ -32,6 +33,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <body className="font-sans antialiased [overflow-wrap:anywhere]">
         {children}
         <Footer />
+        <Analytics />
         <TanStackDevtools
           config={{
             position: 'bottom-right',
