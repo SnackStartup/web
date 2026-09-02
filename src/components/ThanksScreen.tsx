dd1958b946c36dd2e9bc6b063e7dd39827c55694
Matt <64214044+kvbc@users.noreply.github.com>
2026-09-02 15:44:27 +0200

    stuff

---
+++ src/components/ThanksScreen.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react'
+import { FaHeart } from 'react-icons/fa6'
+
+import './ThanksScreen.css'
+
+type Props = {
+  visible: boolean
+  onVisibleChange: (visible: boolean) => void
+}
+
+export const ThanksScreen: React.FC<Props> = ({ visible, onVisibleChange }) => {
+  useEffect(() => {
+    if (!visible) return
+    const timeout = setTimeout(() => {
+      onVisibleChange(false)
+    }, 5000)
+    return () => clearTimeout(timeout)
+  }, [visible])
+
+  return (
+    <div
+      className={`
+          absolute inset-0 bg-white
+          flex flex-col items-center justify-center gap-2
+          transition-opacity duration-500
+          pointer-events-none
+          ${visible ? 'opacity-100' : 'opacity-0'}
+        `}
+    >
+      {visible && (
+        <div className="absolute left-0 -top-1 h-3 w-screen overflow-hidden rounded-full bg-primary animate-countdown" />
+      )}
+      <FaHeart className="size-20 text-pink-500 animate-bounce" />
+      <h1 className="text-primary text-3xl font-bold">Dziękujemy!</h1>
+      <h2 className="text-neutral-400">Zgłoszenie zostało przyjęte</h2>
+    </div>
+  )
+}
--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -11,7 +11,6 @@
 import { Route as rootRouteImport } from './routes/__root'
 import { Route as IndexRouteImport } from './routes/index'
 import { Route as ScannedRouteImport } from './routes/scanned'
-import { Route as ThanksRouteImport } from './routes/thanks'
 
 const IndexRoute = IndexRouteImport.update({
   id: '/',
@@ -23,40 +22,31 @@ const ScannedRoute = ScannedRouteImport.update({
   path: '/scanned',
   getParentRoute: () => rootRouteImport,
 } as any)
-const ThanksRoute = ThanksRouteImport.update({
-  id: '/thanks',
-  path: '/thanks',
-  getParentRoute: () => rootRouteImport,
-} as any)
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
   '/scanned': typeof ScannedRoute
-  '/thanks': typeof ThanksRoute
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
   '/scanned': typeof ScannedRoute
-  '/thanks': typeof ThanksRoute
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
   '/': typeof IndexRoute
   '/scanned': typeof ScannedRoute
-  '/thanks': typeof ThanksRoute
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/scanned' | '/thanks'
+  fullPaths: '/' | '/scanned'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/scanned' | '/thanks'
-  id: '__root__' | '/' | '/scanned' | '/thanks'
+  to: '/' | '/scanned'
+  id: '__root__' | '/' | '/scanned'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
   ScannedRoute: typeof ScannedRoute
-  ThanksRoute: typeof ThanksRoute
 }
 
 declare module '@tanstack/react-router' {
@@ -75,20 +65,12 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof ScannedRouteImport
       parentRoute: typeof rootRouteImport
     }
-    '/thanks': {
-      id: '/thanks'
-      path: '/thanks'
-      fullPath: '/thanks'
-      preLoaderRoute: typeof ThanksRouteImport
-      parentRoute: typeof rootRouteImport
-    }
   }
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   ScannedRoute: ScannedRoute,
-  ThanksRoute: ThanksRoute,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
